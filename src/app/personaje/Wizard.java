9861a1e17cdc1d0b712e970d0bf419ee849a3a26
nenistica <danielamolinaluna1993@gmail.com>
2020-04-29 18:35:43 -0300

    Cambio en Wizard

--- src/app/personaje/Wizard.java
+++ src/app/personaje/Wizard.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 import app.artefacto.Artefacto;
 import app.interfaces.IHaceMagia;
-<<<<<<< HEAD
-=======
-import app.poder.Poder;
->>>>>>> 2ae260f13d67553bffd135a3624c3b9cbbadd877
 import app.transporte.Escoba;
 
 public class Wizard extends Persona implements IHaceMagia {
