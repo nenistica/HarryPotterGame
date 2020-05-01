5a4dc06282b76f2daaaad31e0c1061b77e22ab61
magicmags <magali.bohl150@gmail.com>
2020-05-01 17:34:03 -0300

    agregamos metodos a Criatura que hereda de IEsMagico

--- src/app/personajes/Criatura.java
+++ src/app/personajes/Criatura.java
@@ -3,4 +3,16 @@ package app.personajes;
 import app.interfaces.IEsMagico;
 
 public class Criatura extends Personaje implements IEsMagico {
+
+    @Override
+    public boolean esInvisibleAMuggles() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean esInvisible() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 }
