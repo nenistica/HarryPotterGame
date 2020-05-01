19d4d8077ec45db08fe8eb93c84fd473f6abb845
alexmarygr <alexmarygr@gmail.com>
2020-05-01 17:33:38 -0300

    cambio en criatura

--- src/app/personajes/Criatura.java
+++ src/app/personajes/Criatura.java
@@ -3,4 +3,14 @@ package app.personajes;
 import app.interfaces.IEsMagico;
 
 public class Criatura extends Personaje implements IEsMagico {
+
+    @Override
+    public boolean esInvisibleAMuggles() {
+        return true;
+    }
+
+    @Override
+    public boolean esInvisible() {
+        return true;
+    }
 }
