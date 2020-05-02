689f8e7158282024a9f55dcf2da7db6ec14ca1dc
magicmags <magali.bohl150@gmail.com>
2020-05-02 15:57:51 -0300

    creamos las listas de personajes y hechizos en JuegoHP

--- src/app/JuegoHP.java
+++ src/app/JuegoHP.java
@@ -1,7 +1,18 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class JuegoHP {
  public static Scanner Teclado = new Scanner(System.in);   
+
+ public static List<Personaje> Personajes = new ArrayList<Personaje>();
+ public static List<Hechizo> Hechizos = new ArrayList<Hechizo>();
+
+ public static void agregarPersonajes(){
+
+    Wizard harry = new Wizard()
+ }
+ 
 }
