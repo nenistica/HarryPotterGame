0404151240a683ee56336bc449d84f44695e9d17
nenistica <danielamolinaluna1993@gmail.com>
2020-05-02 20:15:49 -0300

    Organizamos clase Poder.

--- src/app/poderes/Poder.java
+++ src/app/poderes/Poder.java
@@ -1,11 +1,11 @@
 package app.poderes;
 
 public class Poder {
-
+    //Attrs
     String nombrePoder;
-
     String descripcionPoder;
-
+    
+    //Constructores
     public Poder(String nombrePoder, String descripcionPoder) {
         this.nombrePoder = nombrePoder;
         this.descripcionPoder = descripcionPoder;
