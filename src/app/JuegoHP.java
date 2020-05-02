79ceeca96961b2d7a50019169f39742ce1b6945e
magicmags <magali.bohl150@gmail.com>
2020-05-02 19:23:33 -0300

    Eliminamos lista Hechizos.Agregamos Listas de Hechizos Ataque, Curacion, Defensa y  Ocio.

--- src/app/JuegoHP.java
+++ src/app/JuegoHP.java
@@ -3,9 +3,6 @@ package app;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-import javax.swing.RepaintManager;
-
 import app.artefactos.Artefacto;
 import app.artefactos.CapaInvisibilidad;
 import app.personajes.Elfo;
@@ -16,17 +13,21 @@ import app.poderes.Poder;
 import app.poderes.hechizos.Hechizo;
 import app.poderes.hechizos.ataques.AvadaKedavra;
 import app.poderes.hechizos.ataques.Cruciatus;
+import app.poderes.hechizos.ataques.HechizoAtaque;
 import app.poderes.hechizos.ataques.Imperius;
 import app.poderes.hechizos.ataques.PetrificusTotalus;
 import app.poderes.hechizos.ataques.SectumSempra;
 import app.poderes.hechizos.curaciones.Episkey;
+import app.poderes.hechizos.curaciones.HechizoCuracion;
 import app.poderes.hechizos.curaciones.Reparifors;
 import app.poderes.hechizos.curaciones.VulneraSanetur;
 import app.poderes.hechizos.defensas.CaveInimicum;
 import app.poderes.hechizos.defensas.ExpectoPatronum;
 import app.poderes.hechizos.defensas.Expelliarmus;
+import app.poderes.hechizos.defensas.HechizoDefensa;
 import app.poderes.hechizos.defensas.ProtegoMaxima;
 import app.poderes.hechizos.ocios.Alohomora;
+import app.poderes.hechizos.ocios.HechizoOcio;
 import app.poderes.hechizos.ocios.Lumos;
 import app.poderes.hechizos.ocios.WingwardiumLeviosa;
 
@@ -34,7 +35,10 @@ public class JuegoHP {
     public static Scanner Teclado = new Scanner(System.in);
 
     public static List<Personaje> Personajes = new ArrayList<Personaje>();
-    public static List<Hechizo> Hechizos = new ArrayList<Hechizo>();
+    public static List<HechizoAtaque> HechizosAtaque = new ArrayList<HechizoAtaque>();
+    public static List<HechizoDefensa> HechizosDefensa = new ArrayList<HechizoDefensa>();
+    public static List<HechizoCuracion> HechizosCuracion = new ArrayList<HechizoCuracion>();
+    public static List<HechizoOcio> HechizosOcio = new ArrayList<HechizoOcio>();
 
  public static void agregarPersonajes(){
 
@@ -52,49 +56,49 @@ public class JuegoHP {
     public static void agregarHechizos() {
 
         AvadaKedavra avvy = new AvadaKedavra(true, 100, 0, 50);
-        JuegoHP.Hechizos.add(avvy);
+        JuegoHP.HechizosAtaque.add(avvy);
 
         Cruciatus cruccy = new Cruciatus(true, 40, 0, 30);
-        JuegoHP.Hechizos.add(cruccy);
+        JuegoHP.HechizosAtaque.add(cruccy);
 
         Imperius imppy = new Imperius(true, 30, 0, 20);
-        JuegoHP.Hechizos.add(imppy);
+        JuegoHP.HechizosAtaque.add(imppy);
 
         PetrificusTotalus petri = new PetrificusTotalus(false, 20, 0, 10);
-        JuegoHP.Hechizos.add(petri);
+        JuegoHP.HechizosAtaque.add(petri);
 
         SectumSempra sectty = new SectumSempra(false, 25, 0, 15);
-        JuegoHP.Hechizos.add(sectty);
+        JuegoHP.HechizosAtaque.add(sectty);
 
         Episkey eppi = new Episkey(false, 0, 12, 8);
-        JuegoHP.Hechizos.add(eppi);
+        JuegoHP.HechizosCuracion.add(eppi);
 
         Reparifors reppi = new Reparifors(false, 0, 20, 12);
-        JuegoHP.Hechizos.add(reppi);
+        JuegoHP.HechizosCuracion.add(reppi);
 
         VulneraSanetur vully = new VulneraSanetur(false, 0, 30, 20);
-        JuegoHP.Hechizos.add(vully);
+        JuegoHP.HechizosCuracion.add(vully);
 
         CaveInimicum cavvy = new CaveInimicum(false, 30, 20, 40);
-        JuegoHP.Hechizos.add(cavvy);
+        JuegoHP.HechizosDefensa.add(cavvy);
 
         ExpectoPatronum exppy = new ExpectoPatronum(false, 25, 15, 30);
-        JuegoHP.Hechizos.add(exppy);
+        JuegoHP.HechizosDefensa.add(exppy);
 
         Expelliarmus expelly = new Expelliarmus(false, 10, 0, 5);
-        JuegoHP.Hechizos.add(expelly);
+        JuegoHP.HechizosDefensa.add(expelly);
 
         ProtegoMaxima protty = new ProtegoMaxima(false, 15, 20, 15);
-        JuegoHP.Hechizos.add(protty);
+        JuegoHP.HechizosDefensa.add(protty);
 
         Alohomora aloho = new Alohomora(false, 0, 0, 7);
-        JuegoHP.Hechizos.add(aloho);
+        JuegoHP.HechizosOcio.add(aloho);
 
         Lumos lummy = new Lumos(false, 0, 0, 3);
-        JuegoHP.Hechizos.add(lummy);
+        JuegoHP.HechizosOcio.add(lummy);
 
         WingwardiumLeviosa winggy = new WingwardiumLeviosa(false, 0, 0, 9);
-        JuegoHP.Hechizos.add(winggy);
+        JuegoHP.HechizosOcio.add(winggy);
 
     }
 
