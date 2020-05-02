1e3cd1386dce953e98359faa4266795fb4a75b60
magicmags <magali.bohl150@gmail.com>
2020-05-02 15:10:55 -0300

    Importamos teclados en las clases JuegoHP y en App

--- src/app/App.java
+++ src/app/App.java
@@ -1,6 +1,9 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
+    public static Scanner Teclado = new Scanner (System.in);
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
     }
--- src/app/JuegoHP.java
+++ src/app/JuegoHP.java
@@ -1,4 +1,7 @@
 package app;
 
+import java.util.Scanner;
+
 public class JuegoHP {
+ public static Scanner Teclado = new Scanner(System.in);   
 }
